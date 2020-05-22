b890a9cd6da09b7d2d71dd2f9cc1beaf63ca9625
Pinkel Gurung <pinkelgrg@gmail.com>
2020-05-22 18:37:39 -0400

    export BadRequestError

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./errors/error-handler";
 export * from "./errors/request-validation-error";
+export * from "./errors/bad-request-error";
 
 export * from "./middlewares/request-validation";
