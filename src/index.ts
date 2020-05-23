e27acb48021b544f717c6bbf104accee92c12f93
Pinkel Gurung <pinkelgrg@gmail.com>
2020-05-22 23:37:27 -0400

    add 404 error handler

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./errors/error-handler";
 export * from "./errors/request-validation-error";
 export * from "./errors/bad-request-error";
+export * from "./errors/not-found-error";
 
 export * from "./middlewares/request-validation";
